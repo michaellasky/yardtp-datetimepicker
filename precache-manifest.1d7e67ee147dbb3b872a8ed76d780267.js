186dec5a11473e2fc0d2bdfb6878b50484b0226f
mlasky <mlasky@nuclearhorsestudios.com>
2019-02-16 19:27:33 -0500

    Updates

--- precache-manifest.1d7e67ee147dbb3b872a8ed76d780267.js
+++ precache-manifest.1d7e67ee147dbb3b872a8ed76d780267.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "bd96ac598806c4421562",
+    "revision": "8c75f0955fe98274689f",
     "url": "/yardtp-datetimepicker/static/css/main.df67472e.chunk.css"
   },
   {
-    "revision": "bd96ac598806c4421562",
-    "url": "/yardtp-datetimepicker/static/js/main.bd96ac59.chunk.js"
+    "revision": "8c75f0955fe98274689f",
+    "url": "/yardtp-datetimepicker/static/js/main.8c75f095.chunk.js"
   },
   {
     "revision": "e9bc95973543eb157257",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/yardtp-datetimepicker/static/js/2.3e70cdff.chunk.js"
   },
   {
-    "revision": "086ecdb848359b49203fe62d8f317012",
+    "revision": "93a338a1dea88d6839a1710e3b5c2a4e",
     "url": "/yardtp-datetimepicker/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/yardtp-datetimepicker/precache-manifest.a0d795311ba90ee74aa8db53da856829.js"
+  "/yardtp-datetimepicker/precache-manifest.1d7e67ee147dbb3b872a8ed76d780267.js"
 );
 
 workbox.clientsClaim();
